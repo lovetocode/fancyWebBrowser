17040d447a2aebe403217d30cc904efb9e5c90f9
Daniel Thornton <dshaggy28@yahoo.com>
2023-11-14 18:43:56 -0800

    Finished watching video 3.

--- fancy web browser/Control.gd
+++ fancy web browser/Control.gd
@@ -6,14 +6,29 @@ extends Control
 
 
 func _ready():
-	http_request.request("https://godotengine.org/")
+	var default_url = "https://news.ycombinator.com/"
+	address_bar_line_edit.text = default_url
+	http_request.request(default_url)
 	
 
 func _on_http_request_request_completed(result, response_code, headers, body):
-	rich_text_label.text = body.get_string_from_utf8()
+	var html = body.get_string_from_utf8()
+	rich_text_label.text = innterHTML("body", html)
+	DisplayServer.window_set_title(innterHTML("title", html))
+	
 
 
 func _on_go_button_pressed():
 	http_request.request(address_bar_line_edit.text)
+	
+func innterHTML(tag, html, default=""):
+	var regex = RegEx.new()
+	regex.compile("<" + tag + ">(.|\n)*?</" + tag + ">")
+	var result = regex.search(html)
+	if result:
+		return result.get_string().replace("<" + tag + ">", '').replace("</" + tag + ">", '')
+	return default 
+	
+	
 	
 

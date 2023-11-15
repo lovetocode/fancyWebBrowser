dc4d8ca07ffc2981838f4edbf33df1c1e5b4d620
Daniel Thornton <dshaggy28@yahoo.com>
2023-11-14 17:24:24 -0800

    Finished watching first video of tutorial.

--- fancy web browser/Control.gd
+++ fancy web browser/Control.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var http_request = $HTTPRequest
 @onready var rich_text_label = $RichTextLabel
+@onready var address_bar_line_edit = $HBoxContainer/AddressBarLineEdit
 
 
 func _ready():
@@ -10,3 +11,9 @@ func _ready():
 
 func _on_http_request_request_completed(result, response_code, headers, body):
 	rich_text_label.text = body.get_string_from_utf8()
+
+
+func _on_go_button_pressed():
+	http_request.request(address_bar_line_edit.text)
+	
+
